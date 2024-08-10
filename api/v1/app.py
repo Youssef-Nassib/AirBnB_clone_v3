9a03303d543a892801e7cc9d75e2e3d7d217bb61
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-10 18:51:31 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ app - Flask(__name__)
 app.register_blueprint(app_views)
 
 
-if __name == '__main__':
+if __name__ == '__main__':
     HOST = getenv('HBNH_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
