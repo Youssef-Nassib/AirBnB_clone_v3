688d7ad6e464f6230bda0b183629fb2b040c5345
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-10 19:21:53 +0100

    l

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ creat a flask app"""
 
-from flask import jsonif
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status')
 def apiStatus():
     """return OK"""
-
-    response = {'status': "OK"}
-    return jsonify(response)
+    return jsonify({"status": "OK"})
