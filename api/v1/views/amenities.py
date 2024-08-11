b21e2227519f943d4259e10966ff372d10e25af0
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-11 21:10:24 +0100

    amenties

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,11 +14,13 @@ def get_amenitys():
     """
     get the list of all Amenity objects
     """
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict() for amenity in storage.
+                 all(Amenity).values()]
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """
     get a Amenity object
@@ -29,7 +31,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """
     get an Amenity object
@@ -58,7 +61,8 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """
     get an existing Amenity
