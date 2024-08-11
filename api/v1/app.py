0854f341c23751907d57657600166f6bb125d8a4
Youssef-Nassib <wissal2015.nassib@gmail.com>
2024-08-11 21:31:06 +0100

    CORPS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def appTeardown(error):
